3ccd7d8bfbbe41a356a6531ff9edf5c395a823f9
abc <a@b.c>
2016-01-14 06:51:05 +0100

    Estructura:Ex4
    Commit inicial

---
+++ src/main/java/es/egc/App1.java
@@ -0,0 +1,16 @@
+package es.egc;
+
+/**
+ * SUMA
+ *
+ */
+public class App1 
+{
+	
+	
+	public static int suma(int a, int b){
+		int result= a+b;
+		System.out.println("Returning "+a+"+"+b+"="+result);
+		return result;
+	}
+}
---
+++ src/main/java/es/egc/App2.java
@@ -0,0 +1,16 @@
+package es.egc;
+
+/**
+ * RESTA
+ *
+ */
+public class App2 
+{
+	
+	
+	public static int resta(int a, int b){
+		int result= a-b;
+		System.out.println("Returning "+a+"-"+b+"="+result);
+		return result;
+	}
+}
